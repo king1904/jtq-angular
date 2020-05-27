9e194e899f25cd991c0f69cbd05aeaaeba299e31
Youcef Chaala <youcef1904@gmail.com>
2020-05-27 12:27:03 +0200

    make it better4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
  
 export const environment: {production: boolean, baseUrlRestServices: string} = {
   production: true,
+  
   baseUrlRestServices: 'http://jtq-backend.herokuapp.com/services/rest'
 };
